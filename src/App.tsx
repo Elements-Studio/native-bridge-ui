68e15837164f516f1e02a6798312c81ecb88c3ea
耗子 <syntaxright@gmail.com>
2026-01-09 23:47:13 +0800

    feat: 🎸 Initialization

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import '@/App.css'
+import Layout from '@/Layout'
+import { AppRoutes } from '@/routes'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <AppRoutes />
+      </Layout>
+    </BrowserRouter>
+  )
+}
+
+export default App
