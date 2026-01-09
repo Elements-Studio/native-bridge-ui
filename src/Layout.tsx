8bb7af4ea29a2db400d2819dfcd98fb6e5f23612
haozi <syntaxright@gmail.com>
2026-01-09 23:44:56 +0800

    feat: 🎸 Initialization

---
+++ src/Layout.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+export default function Layout(props: { children: React.ReactNode }) {
+  return (
+    <>
+      <header className="mb-4 flex items-center justify-between border-b border-b-gray-300 p-4">
+        <h1><Link to="/">Bridge</Link></h1>
+        <nav>
+          <Link to="/">Bridge</Link>
+          <Link to="/transactions">Transactions</Link>
+        </nav>
+      </header>
+      <main className="h-[calc(100vh-126px)]"> {props.children} </main>
+      <footer className="border-t border-gray-300 p-4">Footer</footer>
+    </>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+})
