386853d231bee1eb7b4c1bdbc35b63603870dac7
Airen <airenliao@gmail.com>
2026-01-25 17:16:01 +0800

    update style for homepage

--- src/pages/Transactions/[:txnHash]/page.tsx
+++ src/pages/Transactions/[:txnHash]/page.tsx
@@ -560,9 +560,9 @@ export default function TransactionsDetailPage() {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       className="h-3 w-3"
                     >
                       <path d="M5 12h14"></path>
@@ -604,9 +604,9 @@ export default function TransactionsDetailPage() {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       className="h-3 w-3"
                     >
                       <path d="M5 12h14"></path>
@@ -699,9 +699,9 @@ export default function TransactionsDetailPage() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             className="h-3 w-3"
                           >
                             <path d="M5 12h14"></path>
