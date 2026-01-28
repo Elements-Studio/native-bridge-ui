4a39e73d425969354e319f14efb4241d214e5eaa
haozi <syntaxright@gmail.com>
2026-01-28 18:59:15 +0800

    feat: 🎸 Update env

--- src/env.production.ts
+++ src/env.production.ts
@@ -1,3 +1,5 @@
+import usdtIcon from '@/assets/img/usdt.svg'
+
 const indexerHost = 'https://bridge-indexer-monitor-dev2anvil.starswap.xyz'
 const signHost = 'https://bridge-sign-monitor-dev2anvil.starswap.xyz'
 
@@ -11,6 +13,26 @@ export default {
         `${signHost}/bridge-node2-dev2anvil/sign`,
         `${signHost}/bridge-node3-dev2anvil/sign`,
       ],
+    },
+  },
+  coins: {
+    USDT: {
+      icon: usdtIcon,
+      name: 'USDT',
+      walletType: 'EVM',
+      network: { name: 'mainnet', chainId: '0x1' },
+      gas: 'ETH',
+      ca: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+    },
+
+    StarUSDT: {
+      icon: usdtIcon,
+      name: 'StarUSDT',
+      walletType: 'STARCOIN',
+      network: { name: 'devnet', chainId: '0x1' },
+      gas: 'STC',
+      ca: '0x9601de11320713ac003a6e41ab8b7dae::USDT::USDT',
+      // ca: '0x9601de11320713ac003a6e41ab8b7dae:Bridge::USDT::USDT'
     },
   },
 }
