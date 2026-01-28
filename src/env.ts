bf6ccb0bde4b3d4a3f943d73d6358219f0bb5d38
haozi <syntaxright@gmail.com>
2026-01-28 22:56:10 +0800

    fix: 🐛 Fix types Error

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,32 @@ import devEnv from './env.development'
 import prodEnv from './env.production'
 import type { Coins } from './types/domain'
 
-const env = (process.env.NODE_ENV === 'development' ? devEnv : prodEnv) as { coins: Coins }
+const env = (process.env.NODE_ENV === 'development' ? devEnv : prodEnv) as {
+  apis: {
+    '/transfers': string
+    '/estimate_fees': string
+    committees: {
+      signs: string[]
+    }
+  }
+  coins: Coins
+  bridge: {
+    evm: {
+      chainIdHex: string
+      chainId: number
+      bridgeAddress: string
+      destinationChainId: number
+    }
+    starcoin: {
+      packageAddress: string
+      chainId: number
+    }
+    tokens: {
+      AnvilUSDT: { tokenId: number; claimFunction: 'claim_bridge_usdt' }
+      SepoliaUSDT: { tokenId: number; claimFunction: 'claim_bridge_usdt' }
+      StarUSDT: { tokenId: number; claimFunction: 'claim_bridge_usdt'; sendFunction: 'send_bridge_usdt'; decimals: number }
+    }
+  }
+}
 
 export default env
