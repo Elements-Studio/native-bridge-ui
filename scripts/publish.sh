a024e5e91481d42aedac47ff4876b67bf48432b5
haozi <syntaxright@gmail.com>
2026-01-28 19:40:34 +0800

    feat: 🎸 No commit.gpgsign

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 root=`pwd`
 
 pnpm run build
@@ -6,7 +8,7 @@ cd $root/dist
 cp index.html 404.html
 git init -b gh-pages
 git remote add origin git@github.com:Elements-Studio/native-bridge-ui.git
-git config user.signingkey C01819ABF74FEBBB
+git config commit.gpgsign false
 git add .
-git commit -S -m "Deploy to GitHub Pages"
+git commit -m "Deploy to GitHub Pages"
 git push  --force --set-upstream origin gh-pages
