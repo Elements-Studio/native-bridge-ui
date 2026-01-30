2d211a67c4d038579f74cf2c35468da89b9cf649
haozi <syntaxright@gmail.com>
2026-01-30 09:39:28 +0800

    refactor: 💡 Refactor Transactions code

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import BridgeAssets from '@/pages/BridgeAssets'
 import TransactionsDetailPage from '@/pages/Transactions/[:txnHash]/page'
 import TransactionsPage from '@/pages/Transactions/page'
+// import NewTx from '@/pages/tx/[:txnHash]/page'
 import { Route, Routes } from 'react-router-dom'
 
 export function AppRoutes() {
@@ -9,6 +10,7 @@ export function AppRoutes() {
       <Route path="/" element={<BridgeAssets />} />
       <Route path="/transactions" element={<TransactionsPage />} />
       <Route path="/transactions/:txnHash" element={<TransactionsDetailPage />} />
+      {/* <Route path="/tx/:txnHash" element={<NewTx />} /> */}
     </Routes>
   )
 }
