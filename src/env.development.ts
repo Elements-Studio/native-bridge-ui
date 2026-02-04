999528e18af0053c2849e2fea233fdfdcfd25686
ValentineReese <vreese177@gmail.com>
2026-02-04 17:27:37 +0800

    change local to halley-sepolia

--- src/env.development.ts
+++ src/env.development.ts
@@ -1,52 +1,52 @@
 import ethIcon from '@/assets/img/eth.svg'
-import sepoliaEthIcon from '@/assets/img/sepolia_eth.svg'
+// import sepoliaEthIcon from '@/assets/img/sepolia_eth.svg'
 import sepoliaUsdtIcon from '@/assets/img/sepolia_usdt.svg'
 import ststarcoinIcon from '@/assets/img/starcoin.svg'
 import usdtIcon from '@/assets/img/usdt.svg'
-import wbtcIcon from '@/assets/img/wbtc.svg'
+// import wbtcIcon from '@/assets/img/wbtc.svg'
 import type { Coins } from '@/types/domain'
 
 export default {
   apis: {
-    '/transfers': `https://bridge-indexer-monitor-dev2anvil.starswap.xyz/transfers`,
-    '/estimate_fees': `https://bridge-node1-dev2anvil.starswap.xyz/estimate_fees`,
+    '/transfers': `https://bridge-indexer-monitor-halley2sepolia.starswap.xyz/transfers`,
+    '/estimate_fees': `https://bridge-node1-halley2sepolia.starswap.xyz/estimate_fees`,
     committees: {
       signs: [
-        `https://bridge-node1-dev2anvil.starswap.xyz/sign`,
-        `https://bridge-node2-dev2anvil.starswap.xyz/sign`,
-        `https://bridge-node3-dev2anvil.starswap.xyz/sign`,
+        `https://bridge-node1-halley2sepolia.starswap.xyz/sign`,
+        `https://bridge-node2-halley2sepolia.starswap.xyz/sign`,
+        `https://bridge-node3-halley2sepolia.starswap.xyz/sign`,
       ],
     },
   },
 
   coins: {
-    ETH: {
-      icon: ethIcon,
-      name: 'ETH',
-      walletType: 'EVM',
-      network: { name: 'mainnet', chainId: '0x1' },
-      gas: 'Gwei',
-      ca: null,
-      decimals: 18,
-    },
-    USDT: {
-      icon: usdtIcon,
-      name: 'USDT',
-      walletType: 'EVM',
-      network: { name: 'mainnet', chainId: '0x1' },
-      gas: 'Gwei',
-      ca: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
-      decimals: 6,
-    },
-    SepoliaETH: {
-      icon: sepoliaEthIcon,
-      name: 'SepoliaETH',
-      walletType: 'EVM',
-      network: { name: 'sepolia', chainId: '0xaa36a7' },
-      gas: 'Gwei',
-      ca: null,
-      decimals: 18,
-    },
+    // ETH: {
+    //   icon: ethIcon,
+    //   name: 'ETH',
+    //   walletType: 'EVM',
+    //   network: { name: 'mainnet', chainId: '0x1' },
+    //   gas: 'Gwei',
+    //   ca: null,
+    //   decimals: 18,
+    // },
+    // USDT: {
+    //   icon: usdtIcon,
+    //   name: 'USDT',
+    //   walletType: 'EVM',
+    //   network: { name: 'mainnet', chainId: '0x1' },
+    //   gas: 'Gwei',
+    //   ca: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    //   decimals: 6,
+    // },
+    // SepoliaETH: {
+    //   icon: sepoliaEthIcon,
+    //   name: 'SepoliaETH',
+    //   walletType: 'EVM',
+    //   network: { name: 'sepolia', chainId: '0xaa36a7' },
+    //   gas: 'Gwei',
+    //   ca: null,
+    //   decimals: 18,
+    // },
     SepoliaUSDT: {
       icon: sepoliaUsdtIcon,
       name: 'SepoliaUSDT',
@@ -56,34 +56,34 @@ export default {
       ca: '0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0',
       decimals: 6,
     },
-    AnvilETH: {
-      icon: ethIcon,
-      name: 'AnvilETH',
-      walletType: 'EVM',
-      network: { name: 'anvil', chainId: '0x7a69' },
-      gas: 'Gwei',
-      ca: null,
-      decimals: 18,
-    },
-    AnvilUSDT: {
-      isDefault: true,
-      icon: usdtIcon,
-      name: 'AnvilUSDT',
-      walletType: 'EVM',
-      network: { name: 'anvil', chainId: '0x7a69' },
-      gas: 'Gwei',
-      ca: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
-      decimals: 6,
-    },
-    WBTC: {
-      icon: wbtcIcon,
-      name: 'WBTC',
-      walletType: 'EVM',
-      network: { name: 'mainnet', chainId: '0x1' },
-      gas: 'Gwei',
-      ca: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
-      decimals: 8,
-    },
+    // AnvilETH: {
+    //   icon: ethIcon,
+    //   name: 'AnvilETH',
+    //   walletType: 'EVM',
+    //   network: { name: 'anvil', chainId: '0x7a69' },
+    //   gas: 'Gwei',
+    //   ca: null,
+    //   decimals: 18,
+    // },
+    // AnvilUSDT: {
+    //   isDefault: true,
+    //   icon: usdtIcon,
+    //   name: 'AnvilUSDT',
+    //   walletType: 'EVM',
+    //   network: { name: 'anvil', chainId: '0x7a69' },
+    //   gas: 'Gwei',
+    //   ca: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+    //   decimals: 6,
+    // },
+    // WBTC: {
+    //   icon: wbtcIcon,
+    //   name: 'WBTC',
+    //   walletType: 'EVM',
+    //   network: { name: 'mainnet', chainId: '0x1' },
+    //   gas: 'Gwei',
+    //   ca: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    //   decimals: 8,
+    // },
     // STC: {
     //   icon: usdtIcon,
     //   name: 'STC',
@@ -96,23 +96,23 @@ export default {
       icon: usdtIcon,
       name: 'StarUSDT',
       walletType: 'STARCOIN',
-      network: { name: 'devnet', chainId: '0x1' },
+      network: { name: 'halley', chainId: '0x1' },
       gas: 'nanoSTC',
-      ca: '0x7db7d4c5322285b8357b81aaaa1aba09::USDT::USDT',
+      ca: '0x93c6a07496c2d1103cea43abc3e233c2::USDT::USDT',
       decimals: 6,
     },
   } satisfies Coins,
 
   bridge: {
     evm: {
-      chainIdHex: '0x7a69',
-      chainId: 12,
-      bridgeAddress: '0x0B306BF915C4d645ff596e518fAf3F9669b97016',
-      destinationChainId: 2,
+      chainIdHex: '0xaa36a7',
+      chainId: 11,
+      bridgeAddress: '0xEfd2BB03e5A5ceCC567D13E0baB1eeeCAB38D55a',
+      destinationChainId: 1,
     },
     starcoin: {
-      packageAddress: '0x7db7d4c5322285b8357b81aaaa1aba09',
-      chainId: 2,
+      packageAddress: '0x93c6a07496c2d1103cea43abc3e233c2',
+      chainId: 1,
     },
     tokens: {
       AnvilUSDT: { tokenId: 4, claimFunction: 'claim_bridge_usdt' },
--- src/types/domain.ts
+++ src/types/domain.ts
@@ -32,7 +32,7 @@ export type CoinItem = {
   // 链类型
   walletType: WalletType
   network: {
-    name: 'mainnet' | 'sepolia' | 'anvil' | 'devnet'
+    name: 'mainnet' | 'sepolia' | 'anvil' | 'devnet' | 'halley'
     chainId: string // '0x1' | '0xfb' | '0x6a'
   }
   gas: 'ETH' | 'SepoliaETH' | 'STC' | 'AnvilETH' | 'Gwei' | 'nanoSTC'
